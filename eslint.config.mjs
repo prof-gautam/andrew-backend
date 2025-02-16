bb608633ef15ec2ed1801ea3f36a02d3bc328786
Gautam Phuyal <profphuyal@gmail.com>
2025-02-15 16:15:17 -0800

    Modifies Eslint Config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,21 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+/** @type {import('eslint').Linter.Config} */
+export default {
+  languageOptions: {
+    globals: globals.browser,
+  },
+  rules: {
+    "no-unused-vars": "warn",
+  },
+  overrides: [
+    {
+      name: "example/strict",
+      rules: {
+        "no-unused-vars": "error",
+      },
+    },
+  ],
+  ...pluginJs.configs.recommended,
+};
